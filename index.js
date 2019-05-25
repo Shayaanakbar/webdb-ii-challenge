4fbf03d69a2ed08fca939726d37b7f5e98f2508c
shayaanakbar <shayaanakbar1106@gmail.com>
2019-05-25 16:22:22 -0500

    Hit mvp

--- index.js
+++ index.js
@@ -47,6 +47,57 @@ server.get('/api/zoos/:id', (req,res) => {
     });
 });
 
+server.post('/api/zoos', (req, res) => {
+  db('zoos')
+    .insert(req.body, 'id')
+    .then(ids => {
+      db('zoos')
+        .where({id: ids[0] })
+        .first()
+        .then(zoo => {
+          res.status(200).json(zoo);
+        })
+        .catch(err => {
+          res.status(500).json(err);
+        })
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.put('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .update(req.body)
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({message: `${count} ${count > 1 ? 'records' : 'record'} updated`,});
+      } else {
+        res.status(404).json({message: 'zoo does not exist '})
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+server.delete('/api/zoos/:id', (req, res) => {
+  db('zoos')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({message: `${count} ${count > 1 ? 'records' : 'record'} updated`,});
+      } else {
+        res.status(404).json({message: 'Role does not exist '})
+      }
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
 
 const port = 3300;
 server.listen(port, function() {
